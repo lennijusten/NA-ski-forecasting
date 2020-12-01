f119bc27d223ef839c02e3f2512aa67714c36ef8
lennijusten <ljusten@wisc.edu>
2020-12-01 15:36:02 -0600

    Added dask support for distributed computing

--- wetbulb-metpy.py
+++ wetbulb-metpy.py
@@ -3,20 +3,29 @@ import pandas as pd
 import metpy.calc
 import xarray as xr
 import matplotlib.pyplot as plt
+from dask.distributed import Client, LocalCluster
+
+# Setup adaptive dask client
+cluster = LocalCluster(dashboard_address=':8901',memory_limit='5GB',threads_per_worker=1 )
+cluster.adapt(minimum=1, maximum=10)  # scale between 0 and 100 workers
+client=Client(cluster)
 
 
-def init_data():
+def init_data(chunks_dict):
     ds_press = xr.open_dataset(
-        '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/monthly/PS/b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h0.PS.185001-200512.nc')
+        '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/monthly/PS/b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h0.PS.185001-200512.nc',
+        chunks=chunks_dict)
     ds_temp = xr.open_dataset(
-        '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/daily/TREFHT/b.e11.BRCP85C5CNBDRD.f09_g16.001.cam.h1.TREFHT.20810101-21001231.nc')
+        '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/daily/TREFHT/b.e11.BRCP85C5CNBDRD.f09_g16.001.cam.h1.TREFHT.20810101-21001231.nc',
+        chunks=chunks_dict)
     ds_qbot = xr.open_dataset(
-        '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/daily/QBOT/b.e11.BRCP85C5CNBDRD.f09_g16.001.cam.h1.QBOT.20810101-21001231.nc')
+        '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/daily/QBOT/b.e11.BRCP85C5CNBDRD.f09_g16.001.cam.h1.QBOT.20810101-21001231.nc',
+        chunks=chunks_dict)
 
     return ds_temp, ds_press, ds_qbot
 
 
-temp, press, qbot = init_data()
+temp, press, qbot = init_data(chunks_dict={'lat': 20, 'lon': 20})
 
 
 def select_grid(temp, press, qbot, bot_lat, top_lat, left_lon, right_lon):
@@ -57,3 +66,7 @@ def get_dewpoint(temp,press,qbot,method):
 vapor_pressure, dewpointB = get_dewpoint(temp2, press_mean, qbot2, 'WVPP')
 
 wetbulb = metpy.calc.wet_bulb_temperature(press_mean['PS'], temp2['TREFHT'], dewpointB)
+print(wetbulb)
+wetbulb.to_netcdf('wetbulb.nc')
+
+client.close()
