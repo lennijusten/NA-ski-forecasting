d1400be91eb9ba9ba6d4410eb2d7e76e79cd88ba
lennijusten <65619456+lennijusten@users.noreply.github.com>
2021-04-11 11:41:33 -0500

    Update wetbulb_calc_ensemble_from_reference_grid.py

--- wetbulb_calc_ensemble_from_reference_grid.py
+++ wetbulb_calc_ensemble_from_reference_grid.py
@@ -1,6 +1,8 @@
 # Calculates wetbulb for an entire ensemble member and returns a single netCDF file
 # Runtime ~1hour
 
+# Author: Lennart Justen
+
 import numpy as np
 import matplotlib.pyplot as plt
 import metpy.calc
