917b2c3299fde120a249aaa3e1454bb58ec9cea2
lennijusten <65619456+lennijusten@users.noreply.github.com>
2021-04-11 10:47:33 -0500

    Add files via upload

---
+++ wetbulb_highres.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import metpy.calc
+from metpy.units import units
+import pickle
+
+press_min = 51000 # Pa
+press_max = 105000 # Pa
+press_nlayers = 300
+
+temp_min = -81.0 # degC
+temp_max = 48.0 # degC
+temp_res = 0.5
+
+dewpoint_min = -81.0
+dewpoint_max = 30.0 
+dewpoint_res = 0.5
+
+
+t = np.arange(temp_min, temp_max, temp_res)
+td = np.arange(dewpoint_min, dewpoint_max, dewpoint_res)
+p = np.linspace(press_min, press_max, press_nlayers)
+
+
+tt,td,pp = np.meshgrid(t,td,p)
+
+wetbulb = metpy.calc.wet_bulb_temperature(pp * units.Pa, tt * units.degC, td*units.degC)
+
+with open('wetbulb_metpy_300_0.5_grid.pkl', 'wb') as f:
+    pickle.dump(wetbulb_all, f)
