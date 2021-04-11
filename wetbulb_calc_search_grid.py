d6292e9300f2eb3b0388ef662bcd71586e46097e
lennijusten <65619456+lennijusten@users.noreply.github.com>
2021-04-11 10:49:31 -0500

    Update and rename wetbulb_highres.py to wetbulb_calc_search_grid.py

--- wetbulb_calc_search_grid.py
+++ wetbulb_calc_search_grid.py
@@ -1,3 +1,6 @@
+# Calculate reference grid at specified resoulution
+# WARNING: Long run times (5+ days)
+
 import numpy as np
 import matplotlib.pyplot as plt
 import metpy.calc
@@ -16,6 +19,19 @@ dewpoint_min = -81.0
 dewpoint_max = 30.0 
 dewpoint_res = 0.5
 
+# LowRes Grid Parameters
+
+# press_min = 51000 # Pa
+# press_max = 105000 # Pa
+# press_nlayers = 200
+
+# temp_min = -81.0 # degC
+# temp_max = 48.0 # degC
+# temp_res = 1.0
+
+# dewpoint_min = -81.0
+# dewpoint_max = 30.0 
+# dewpoint_res = 1.0
 
 t = np.arange(temp_min, temp_max, temp_res)
 td = np.arange(dewpoint_min, dewpoint_max, dewpoint_res)
@@ -27,4 +43,4 @@ tt,td,pp = np.meshgrid(t,td,p)
 wetbulb = metpy.calc.wet_bulb_temperature(pp * units.Pa, tt * units.degC, td*units.degC)
 
 with open('wetbulb_metpy_300_0.5_grid.pkl', 'wb') as f:
-    pickle.dump(wetbulb_all, f)
+    pickle.dump(wetbulb, f)
