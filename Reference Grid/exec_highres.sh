effc6f1c0d70bd3abe90decd5037569dc73c69ee
lennijusten <65619456+lennijusten@users.noreply.github.com>
2021-04-11 11:45:02 -0500

    Add files via upload

---
+++ Reference Grid/exec_highres.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+ENVNAME=Climate
+ENVDIR=$ENVNAME
+
+export PATH
+mkdir $ENVDIR
+tar -xzf $ENVNAME.tar.gz -C $ENVDIR
+. $ENVDIR/bin/activate
+
+ls -alF
+
+python3 wetbulb_highres.py
