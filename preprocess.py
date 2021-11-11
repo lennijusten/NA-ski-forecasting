3bb55db616cfee885da7593e1f07a520701834f6
lennijusten <ljusten@wisc.edu>
2021-11-10 22:29:12 -0600

    Upload ensemble processing files

---
+++ preprocess.py
@@ -0,0 +1,103 @@
+# Description: refactor the files from emaroon such that temp and qbot are represented by single files for each
+# ensemble. Additionally, restrict the temporal and spatial domain of the refactored files with the config dict.
+# The script also prepares the pressure file by taking the time mean over the press_path dataset.
+
+# Author: Lenni Justen
+# Date: 11/10/21
+
+from tqdm import tqdm
+import xarray as xr
+import glob
+import cftime
+import os
+
+press_path = '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/monthly/PS/b.e11.B20TRC5CNBDRD.f09_g16.001.cam.h0.PS.185001-200512.nc'
+trefht_path = '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/daily/TREFHT/'
+qbot_path = '/adhara_a/emaroon/cesmLE1/atm/proc/tseries/daily/QBOT/'
+grid_path = '/home1/ljusten/wetbulb_metpy_lowres_2_15_grid.pkl'
+
+trefht_save_path = '/adhara_a/ljusten/TREFHT/'
+qbot_save_path = '/adhara_a/ljusten/QBOT/'
+
+ensmems = list(range(1, 36, 1)) + list(range(101, 106, 1))
+
+
+config = {
+    'start': cftime.DatetimeNoLeap(1920, 1, 1, 0, 0, 0),
+    'end': cftime.DatetimeNoLeap(2100, 12, 31, 0, 0, 0),
+    'lat': (23, 72),
+    'lon': (190, 295),
+}
+
+
+def preprocess(ds):
+    ds_t = ds.sel(time=slice(config['start'], config['end']))
+    ds_g = ds_t.sel(lat=slice(config['lat'][0], config['lat'][1])).sel(lon=slice(config['lon'][0], config['lon'][1]))
+    return ds_g
+
+
+def refactor_dataset(path, data_vars):
+    openme = [sorted(glob.glob(path + 'b.e11.B[2,R]*.' + str(ii).zfill(3) + '*.nc')) for ii in ensmems]
+
+    for mem, oo in tqdm(enumerate(openme)):
+        save_name = '/adhara_a/ljusten/{}/b.e11.BRCP85C5CNBDRD.f09_g16.{}.cam.h1.{}.19200101-21001231.nc'.format(
+            data_vars[0], ensmems[mem], data_vars[0])
+
+        try:
+            os.remove(save_name)
+        except OSError:
+            pass
+
+        try:
+            ds = xr.open_mfdataset(oo, combine='by_coords', compat='override', coords='minimal', data_vars=data_vars,
+                                   preprocess=preprocess, chunks={})
+
+            ds.to_netcdf(save_name)
+        except:
+            print('WARNING: Ensemble member {} could not be concatenated. Trying naive concat method...'.format(mem))
+
+            ds_ = []
+            for f in openme[mem]:
+                ds_.append(xr.open_dataset(f))
+
+            ds = xr.concat(ds_, dim='time', compat='override', coords='minimal', data_vars=data_vars)
+            ds1 = ds.sel(lat=slice(config['lat'][0], config['lat'][1])).sel(
+                lon=slice(config['lon'][0], config['lon'][1]))
+
+            ds1.to_netcdf(save_name)
+
+
+refactor_dataset(qbot_path, ['QBOT'])
+refactor_dataset(trefht_path, ['TREFHT'])
+
+
+def single_file_save(files, data_var):
+    ds = []
+    for f in tqdm(files):
+        ds.append(xr.open_dataset(f))
+
+    return xr.concat(ds, dim='M', data_vars=[data_var], compat='override', coords='minimal')
+
+
+# ds_temp = single_file_save(glob.glob(os.path.join(trefht_save_path,'*.nc')), 'TREFHT')
+# ds_temp.drop(['cosp_sr', 'cosp_prs', 'cosp_tau', 'cosp_ht','cosp_tau_modis', 'cosp_htmisr', 'cosp_scol', 'cosp_sza'])
+# ds_temp.to_netcdf('/adhara_a/ljusten/b.e11.BRCP85C5CNBDRD.f09_g16.ALL.cam.h1.TREFHT.19200101-21001231.nc')
+#
+#
+# ds_qbot = single_file_save(glob.glob(os.path.join(qbot_save_path,'*.nc')), 'QBOT')
+# ds_qbot.drop(['cosp_sr', 'cosp_prs', 'cosp_tau', 'cosp_ht','cosp_tau_modis', 'cosp_htmisr', 'cosp_scol', 'cosp_sza'])
+# ds_qbot.to_netcdf('/adhara_a/ljusten/b.e11.BRCP85C5CNBDRD.f09_g16.ALL.cam.h1.QBOT.19200101-21001231.nc')
+
+
+def pressure_prep(path):
+    press = xr.open_dataset(path)
+
+    press_ = preprocess(press)
+
+    press_mean = press_.mean('time')
+    press_mean['PS'] = press_mean.PS.assign_attrs(units='Pa')
+    return press_mean
+
+
+ds_press = pressure_prep(press_path)
+ds_press.to_netcdf('/adhara_a/ljusten/b.e11.B20TRC5CNBDRD.f09_g16.MEAN-001.cam.h0.PS.185001-200512.nc')
---
+++ wetbulb_calc_from_reference_grid-ensemble.py
@@ -0,0 +1,132 @@
+# Description: calculate wetbulb temperature from the refactored files of the preprocess.py script. Wetbulb is
+# calculated using the low-res reference grid.
+
+# Author: Lenni Justen
+# Date 11/10/21
+
+import numpy as np
+import metpy.calc
+from tqdm import tqdm
+import pickle
+import xarray as xr
+from natsort import natsorted
+import glob
+
+temp_path = '/adhara_a/ljusten/TREFHT/*.nc'
+qbot_path = '/adhara_a/ljusten/QBOT/*.nc'
+press_path = '/adhara_a/ljusten/b.e11.B20TRC5CNBDRD.f09_g16.MEAN-001.cam.h0.PS.185001-200512.nc'
+
+ensmems = list(range(1, 36, 1)) + list(range(101, 106, 1)) # Define list of ensemble member IDs
+
+
+def init_data(temp_path, qbot_path, press_path):
+    # Purpose: open temp and qbot files into a list of ensemble members and return. Open the press datset and return.
+
+    press = xr.open_dataset(press_path)
+    temp = []
+    qbot = []
+
+    for ft, fq in tqdm(zip(natsorted(glob.glob(temp_path)), natsorted(glob.glob(qbot_path)))):
+        temp.append(xr.open_dataset(ft))
+        qbot.append(xr.open_dataset(fq))
+
+    return temp, press, qbot
+
+
+temp, press, qbot = init_data(temp_path, qbot_path, press_path)
+
+
+def load_reference_grid(path):
+    with (open(path, "rb")) as openfile:
+        while True:
+            try:
+                wetbulb_grid = pickle.load(openfile)
+            except EOFError:
+                break
+
+    # Params for LowRes grid
+    grid_config = {
+        'press_min': 51000,  # Pa
+        'press_max': 105000,  # Pa
+        'press_nlayers': 200,
+
+        'temp_min': -81.0,  # degC
+        'temp_max': 48.0,  # degC
+        'temp_res': 1.0,
+
+        'dewpoint_min': -81.0,
+        'dewpoint_max': 30.0,
+        'dewpoint_res': 1.0,
+    }
+
+    return np.array(wetbulb_grid), grid_config
+
+
+wetbulb_grid, grid_config = load_reference_grid('/home1/ljusten/wetbulb_metpy_lowres_2_15_grid.pkl')
+
+
+def get_dewpoint(temp, press, qbot, method):
+    if method == 'RH':
+        # RH = metpy.calc.relative_humidity_from_mixing_ratio(qbot['QBOT'], temp['TREFHT'], press['PS'])
+        RH = metpy.calc.relative_humidity_from_mixing_ratio(press['PS'], temp['TREFHT'], qbot['QBOT'])
+        # dewpoint = metpy.calc.dewpoint_from_relative_humidity(temp['TREFHT'], RH)
+        dewpoint = metpy.calc.dewpoint_from_relative_humidity(temp['TREFHT'], RH)
+        return RH, dewpoint
+    elif method == 'WVPP':
+        vapor_pressure = metpy.calc.vapor_pressure(press['PS'], qbot[
+            'QBOT'])  # https://unidata.github.io/MetPy/latest/api/generated/metpy.calc.vapor_pressure.html
+        dewpoint = metpy.calc.dewpoint(vapor_pressure)
+        return vapor_pressure, dewpoint
+    else:
+        print("Unknown method--Relative Humidity: 'RH', Water Vapor Partial Pressure: 'WVPP'")
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+
+def run_harness():
+    # Range of param values (Global)
+    p_space = np.linspace(grid_config['press_min'], grid_config['press_max'], grid_config['press_nlayers'])
+    t_space = np.arange(grid_config['temp_min'], grid_config['temp_max'], grid_config['temp_res'])
+    dp_space = np.arange(grid_config['dewpoint_min'], grid_config['dewpoint_max'], grid_config['dewpoint_res'])
+
+    for i in tqdm(range(len(ensmems[0:5]))):
+        temp_iter = temp[i]
+        qbot_iter = qbot[i]
+
+        RH, dewpoint = get_dewpoint(temp_iter, press, qbot_iter, 'RH')
+
+        temp_degC = temp_iter['TREFHT'].values - 273.15
+
+        long_temp = temp_degC.flatten()
+        long_press = np.tile(press['PS'].values.flatten(), temp_degC.shape[0])
+        long_dewpoint = dewpoint.values.flatten()
+
+        wetbulb_search = []
+        for ii in range(len(long_temp)):
+            p_idx = find_nearest(p_space, long_press[ii])
+            t_idx = find_nearest(t_space, long_temp[ii])
+            dp_idx = find_nearest(dp_space, long_dewpoint[ii])
+            wetbulb_search.append(round(wetbulb_grid[p_idx][dp_idx][t_idx], 4))
+
+        wetbulb = np.reshape(np.array(wetbulb_search), temp_degC.shape)
+
+        wetbulb_ = xr.DataArray(wetbulb, dims=['time', 'lat', 'lon'],
+                                coords={'time': temp_iter.time, 'lat': temp_iter.lat, 'lon': temp_iter.lon})
+        wetbulb_.attrs['units'] = 'degC'
+        wetbulb_.attrs['long_name'] = 'Wetbulb temperature'
+
+        try:
+            wetbulb_ = wetbulb_.rename_vars({"__xarray_dataarray_variable__": "wetbulb"})
+        except:
+            pass
+
+        wetbulb_.to_netcdf(
+            "/adhara_a/ljusten/Wetbulb/b.e11.BRCP85C5CNBDRD.f09_g16.{}.cam.h1.WETBULB.19200101-21001231.nc".format(
+                ensmems[i]))
+
+
+run_harness()
